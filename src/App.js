f14b0719f3eebd344fb203eb70aec7ad7abc05dc
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-14 12:03:43 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
-import Navbar from "./Navbar";
+import Navbar from "./Navbar.js";
 import "./App.css";
 
 const navlinks = [
-  { name: "Home", to: "/" },
-  { name: "About", to: "/about" },
-  { name: "Contact", to: "/contact" }
+  { name: "Home", to: "#/" },
+  { name: "About", to: "#/about" },
+  { name: "Contact", to: "#/contact" }
 ];
-export default class App extends Component {
+
+const brand = { name: "peekaboo", to: "/" };
+export class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "100rem" }}>
-        <Navbar brand={{ name: "peekaboo", to: "home" }} links={navlinks} />
+      <div className="App">
+        <Navbar brand={brand} links={navlinks} />
         <div className="container">
           <div>
             <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
@@ -198,3 +200,4 @@ export default class App extends Component {
     );
   }
 }
+export default App;
